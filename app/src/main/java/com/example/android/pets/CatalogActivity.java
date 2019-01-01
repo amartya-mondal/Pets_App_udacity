1a5b205b6207e76ea65263e21d21027bfb9ebfaf
Amartya Mondal <hayyoulistentome@gmail.com>
2019-01-01 21:10:14 +0530

    Content Provider update 3

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -19,6 +19,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -122,17 +123,14 @@ public class CatalogActivity extends AppCompatActivity {
 
 
     private void insertPet(){
-        SQLiteDatabase db= mDbHelper.getWritableDatabase();
+
         ContentValues values = new ContentValues();
         values.put(PetContract.PetEntry.COLUMN_PET_NAME, "Toto");
         values.put(PetContract.PetEntry.COLUMN_PET_BREED, "Terrier");
         values.put(PetContract.PetEntry.COLUMN_PET_GENDER, PetContract.PetEntry.GENDER_MALE);
         values.put(PetContract.PetEntry.COLUMN_PET_WEIGHT, 7);
 
-        long newRowId=db.insert(PetContract.PetEntry.TABLE_NAME, null, values);
-
-
-
+        Uri newUri=getContentResolver().insert(PetContract.PetEntry.CONTENT_URI,values);
     }
 
 
