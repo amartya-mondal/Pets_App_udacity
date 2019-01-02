a1dce9560451ef1c363ecf8ad8cfd3ba2ef9f0fa
Amartya Mondal <hayyoulistentome@gmail.com>
2019-01-02 09:53:50 +0530

    Added the empty view

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -54,6 +54,14 @@ public class CatalogActivity extends AppCompatActivity {
         });
 
         mDbHelper = new PetDbHelper(this);
+
+
+        // Find the ListView which will be populated with the pet data
+        ListView petListView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
     }
         @Override
         protected void onStart() {
